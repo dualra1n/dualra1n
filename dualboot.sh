044424cac774aa0150a529f34a2c676fdcb1fe1f
edwin <edwingamerpro2004@gmail.com>
2022-12-10 17:16:50 -0400

    update

--- dualboot.sh
+++ dualboot.sh
@@ -21,7 +21,7 @@ disk=8
 extractedIpsw="ipsw/extracted/"
 
 if [ ! -d "ramdisk/" ]; then
-    git clone --recursive https://github.com/edwin170/ramdisk
+    git clone --recursive https://github.com/edwin170/ramdisk.git
 fi
 # =========
 # Functions
