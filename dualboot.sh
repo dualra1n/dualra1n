2119808b5e30a1d365b8e6600a494379b6cbde97
dualra1n <85508740+dualra1n@users.noreply.github.com>
2023-03-06 16:02:48 -0400

    update submodule command

--- dualboot.sh
+++ dualboot.sh
@@ -452,7 +452,7 @@ fi
 
 # Update submodules
 git submodule update --init --recursive 
-git submodule update --remote --merge
+git submodule foreach git pull origin main
 
 # Re-create work dir if it exists, else, make it
 if [ -e work ]; then
